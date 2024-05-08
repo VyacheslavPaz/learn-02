4666bb29bc194634b0327b6d0264a9a47997dd3a
Pazukha Vyacheslav <slava_pazukha@mail.ru>
2024-05-08 15:49:18 +0500

    Add file todo_list.js

--- todo_list.js
+++ todo_list.js
@@ -66,6 +66,26 @@ class TodoApp extends React.Component {
         </ul>
       );
     }
+    async fetchData () {
+      return await fetch('tasks.json')
+                  .then(function(response) {
+                      return response.json();
+                  }).then(function(tasks) {
+                      return tasks;
+                  })          
+  }
+  async componentDidMount() {
+      let tasks = await this.fetchData()
+      let result = []
+      Object.keys(tasks).map(function(objectKey, index) {
+          var value = tasks[objectKey];
+          result.push(value)
+      });
+      
+      this.setState({items: result})
+      
+  }
+
   }
   const domContainer = document.querySelector('#todo_list');
   ReactDOM.render(<TodoApp/>, domContainer);
